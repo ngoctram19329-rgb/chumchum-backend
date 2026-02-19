851abd88b349dd7bd4312533b3fb4b69b2136f2d
ngoctram19329-rgb <ngoctram19329@gmail.com>
2026-02-20 02:10:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+dconst express = require("express");
 const cors = require("cors");
 
 const app = express();
@@ -33,4 +33,20 @@ app.get("/admin/orders", (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running...");
+});
+app.get("/orders", (req, res) => {
+  res.json(orders);
+});
+
+app.post("/approve", (req, res) => {
+  const { id } = req.body;
+  const order = orders.find(o => o.id == id);
+  if (order) {
+    order.status = "done";
+  }
+  res.json({ message: "Đã duyệt" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
 });
