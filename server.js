4896526df8e2a637e2deceecc2b90904088a38f3
ngoctram19329-rgb <ngoctram19329@gmail.com>
2026-02-20 09:16:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,25 +2,20 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
-const ADMIN_EMAIL = "chumchumcte193@gmail.com";
-const ADMIN_PASSWORD = "19329";
 
 app.post("/admin-login", (req, res) => {
   const { email, password } = req.body;
 
-  if (email === ADMIN_EMAIL && password === ADMIN_PASSWORD) {
+  if (email === "admin@gmail.com" && password === "123456") {
     res.json({ success: true });
   } else {
-    res.json({ success: false, message: "Sai email hoặc mật khẩu" });
+    res.json({ success: false });
   }
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+  console.log("Server running on port " + PORT);
 });
